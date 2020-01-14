90ab33ed42bade65ec68426efd91332601eb8bf0
Paulien <paulien.m.73@gmail.com>
2020-01-14 17:15:03 +0100

    Display message on data fetching

--- src/app/service/hero.service.ts
+++ src/app/service/hero.service.ts
@@ -2,13 +2,16 @@ import { Injectable } from '@angular/core';
 import {Hero} from '../data/hero';
 import {HEROES} from '../data/mock-heroes';
 import {Observable, of} from 'rxjs';
+import {MessageService} from './message.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
   getHeroes(): Observable<Hero[]> {
+    // TODO: envoyer un message une fois les héros trouvés
+    this.messageService.add('HeroService: fetched heroes');
     return of(HEROES);
   }
-  constructor() { }
+  constructor(private messageService: MessageService) { }
 }
