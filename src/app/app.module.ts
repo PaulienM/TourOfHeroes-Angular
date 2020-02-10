0b5c751490698b0cdd06fb815505ba9d7387498b
Paulien <paulien.m.73@gmail.com>
2020-02-10 17:18:04 +0100

    Improve list design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,7 @@ import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { MessagesComponent } from './messages/messages.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatButtonModule, MatIconModule, MatMenuModule, MatToolbarModule} from '@angular/material';
+import {MatButtonModule, MatIconModule, MatListModule, MatMenuModule, MatToolbarModule} from '@angular/material';
 import {AngularFireModule} from '@angular/fire';
 import {environment} from '../environments/environment';
 import {AngularFirestoreModule} from '@angular/fire/firestore';
@@ -32,7 +32,9 @@ import {AngularFirestoreModule} from '@angular/fire/firestore';
     MatIconModule,
     MatButtonModule,
     AngularFireModule.initializeApp(environment.firebase),
-    AngularFirestoreModule, // imports firebase/firestore, only needed for database features
+    AngularFirestoreModule,
+    MatListModule,
+    // imports firebase/firestore, only needed for database features
   ],
   providers: [],
   bootstrap: [AppComponent]
