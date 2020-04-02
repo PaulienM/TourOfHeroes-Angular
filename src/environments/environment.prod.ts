05aec971aebbcee7546097446459f1de27f7b1f5
Paulien <paulien.m.73@gmail.com>
2020-04-02 23:23:00 +0200

    Don't ignore environment files

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyBxRCeJJHYQlbXWqM8aznYQCnZaLfVZPq4',
+    authDomain: 'tour-of-heroes-a90b6.firebaseapp.com',
+    databaseURL: 'https://tour-of-heroes-a90b6.firebaseio.com',
+    projectId: 'tour-of-heroes-a90b6',
+    storageBucket: 'tour-of-heroes-a90b6.appspot.com',
+    messagingSenderId: '833995831216',
+    appId: '1:833995831216:web:4b443318912fea9ea5c875'
+  }
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,25 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyBxRCeJJHYQlbXWqM8aznYQCnZaLfVZPq4',
+    authDomain: 'tour-of-heroes-a90b6.firebaseapp.com',
+    databaseURL: 'https://tour-of-heroes-a90b6.firebaseio.com',
+    projectId: 'tour-of-heroes-a90b6',
+    storageBucket: 'tour-of-heroes-a90b6.appspot.com',
+    messagingSenderId: '833995831216',
+    appId: '1:833995831216:web:4b443318912fea9ea5c875'
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
