7b6c2e45b0d762ec4a366e21d74220331b57d4d4
Paulien <paulien.m.73@gmail.com>
2020-04-03 21:56:51 +0200

    Chart in weapon list

--- src/app/armes/armes.component.ts
+++ src/app/armes/armes.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {Arme} from '../data/arme';
 import {ArmeService} from '../service/arme.service';
+import {ChartDataSets, ChartType, RadialChartOptions} from 'chart.js';
+import {Label} from 'ng2-charts';
 
 @Component({
   selector: 'app-armes',
@@ -8,13 +10,38 @@ import {ArmeService} from '../service/arme.service';
   styleUrls: ['./armes.component.scss']
 })
 export class ArmesComponent implements OnInit {
-  armes: Arme[];
+  armes: Array<any> = [];
+  public radarChartOptions: RadialChartOptions = {
+    responsive: true,
+    scale: {
+      ticks: {
+        suggestedMin: -5,
+        suggestedMax: 5
+      }
+    }
+  };
+  public radarChartLabels: Label[] = ['Attaque', 'Esquive', 'Dégats', 'PV'];
+  public radarChartType: ChartType = 'radar';
+  public isLoading = true;
 
   constructor(private armeService: ArmeService) { }
 
   getArmes(): void {
+    this.isLoading = true;
+    const armesWithChartData = [];
     this.armeService.getArmes()
-      .subscribe(armes => this.armes = armes);
+      .subscribe((armes) => {
+        armes.forEach((arme) => {
+          let armeWithChartData: object;
+          const chartData: ChartDataSets[] = [
+            { data: [arme.attaque, arme.esquive, arme.degat, arme.pv], label: arme.name }
+          ];
+          armeWithChartData = {...arme, chartData};
+          armesWithChartData.push(armeWithChartData);
+        });
+        this.armes = armesWithChartData;
+        this.isLoading = false;
+      });
   }
 
   ngOnInit() {
@@ -23,5 +50,6 @@ export class ArmesComponent implements OnInit {
 
   removeArme(arme: Arme) {
     this.armeService.deleteArme(arme.id);
+    this.getArmes();
   }
 }
