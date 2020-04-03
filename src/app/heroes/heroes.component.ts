6d5b44f2e6cafc59ac8abe163c543ccc3d14a546
Paulien <paulien.m.73@gmail.com>
2020-04-04 01:22:22 +0200

    add some comments

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -32,29 +32,41 @@ export class HeroesComponent implements OnInit {
               private armeService: ArmeService) { }
 
   getHeroes(): void {
+    // Les héros n'ont pas encore chargé
     this.isLoading = true;
+    // objet contenant les héro et les données formaté pour etre utilisées dans ng2chart
     const heroesWithChartData = [];
+    // on récupère les héros grace au héro service
     this.heroService.getHeroes()
+      // lorsque la liste de héros est récupérée..
       .subscribe((heroes) => {
+        // On filtre les héro grace au service filtre
         // @ts-ignore
         heroes = this.filterService.filter(heroes, this.filter);
+        // Pour chaque héro
         heroes.forEach((hero) => {
+          // On créé une variable pour stocker les données du héro formatées pour ng2chart
           let heroWithChartData: object;
           const chartData: ChartDataSets[] = [
             { data: [hero.attaque, hero.esquive, hero.degat, hero.pv], label: hero.name }
           ];
+          // On récupère l'arme associée au héro
           this.armeService.getArme(hero.armeId).subscribe(arme => {
+            // Si l'arme existe en base
             if (arme.id !== undefined) {
+              // On ajoute au données ng2chart les données du héro avec son arme
               chartData.push({
                 data: [hero.attaque + arme.attaque, hero.esquive + arme.esquive, hero.degat + arme.degat, hero.pv + arme.pv ],
                 label: hero.name + ' + ' + arme.name
               });
             }
           });
+          // on combine la valeur de hero avec les données du héro formatées pour ng2chart
           heroWithChartData = {...hero, chartData};
           heroesWithChartData.push(heroWithChartData);
         });
         this.heroes = heroesWithChartData;
+        // Le chargement est terminé
         this.isLoading = false;
       });
   }
